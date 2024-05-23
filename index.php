74d0eda3ef8b3c40347d20d1bcc30bb15a83bb73
ebbie <ebbie777@gmail.com>
2024-05-22 22:50:16 -0700

    testing this message

--- index.php
+++ index.php
@@ -5,4 +5,5 @@ echo "<h2>PHP is Fun!</h2>";
 echo "Hello world!<br>";
 echo "I'm about to learn PHP!<br>";
 echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+echo "testing";
 ?>
